99bbc75cd667a04c3e2e99dd9d1ea17e9d689410
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2025-11-13 15:01:44 +0530

    develop booking item in resource service

--- services/resource_service/routes/api.php
+++ services/resource_service/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\BookingItemController;
 
 
 // Resource Service Category Routes
@@ -19,3 +20,11 @@ Route::get('/resources/{id}', [ResourceController::class, 'show']);
 Route::put('/resources/{id}', [ResourceController::class, 'update']);
 Route::post('/resources/{id}', [ResourceController::class, 'update']);
 Route::delete('/resources/{id}', [ResourceController::class, 'destroy']);
+
+// Booking Item Routes
+Route::get('/booking-items', [BookingItemController::class, 'index']);
+Route::get('/booking-items/available', [BookingItemController::class, 'available']);
+Route::post('/booking-items', [BookingItemController::class, 'store']);
+Route::get('/booking-items/{id}', [BookingItemController::class, 'show']);
+Route::put('/booking-items/{id}', [BookingItemController::class, 'update']);
+Route::delete('/booking-items/{id}', [BookingItemController::class, 'destroy']);
