ba1e9555865a3a8f76691cfd2404a1622187ad5a
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2026-01-13 09:46:18 +0530

    update resource tamplate database

--- services/resource_service/database/migrations/2025_11_03_053131_create_template_fields_table.php
+++ services/resource_service/database/migrations/2025_11_03_053131_create_template_fields_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
         Schema::create('template_fields', function (Blueprint $table) {
             $table->id();
             $table->foreignId('template_id')->constrained('resource_templates')->onDelete('cascade');
-            $table->string('field_name'); // User-friendly name (e.g., "Room Capacity")
-            $table->string('field_key'); // Database key (e.g., "room_capacity")
-            $table->enum('field_type', ['text', 'number', 'textarea', 'checkbox', 'image']); // Field types
+            $table->string('field_name'); 
+            $table->string('field_key'); 
+            $table->enum('field_type', ['text', 'number', 'textarea', 'checkbox', 'image']); 
             $table->boolean('is_required')->default(false);
-            $table->integer('order_index')->default(0); // Display order
+            $table->integer('order_index')->default(0);
             $table->string('placeholder')->nullable();
             $table->text('default_value')->nullable();
             $table->timestamps();
